9b785fd4857386fba89abe1c47789c88f2740dda
Jiefzz <kimffy24@gmail.com>
2014-12-03 21:13:00 +0800

    Module.php配置轉移到module.config.php

--- Order/Module.php
+++ Order/Module.php
@@ -2,18 +2,7 @@
 
 namespace Order;
 
-use Zend\ModuleManager\ModuleManager;
-use Order\Service\PersistOrder;
-use Order\Service\PersistMultiOrder;
-
 class Module {
-	public function init(ModuleManager $moduleManager) {
-		/*$sharedEvents = $moduleManager->getEventManager ()->getSharedManager ();
-		$sharedEvents->attach ( __NAMESPACE__, 'dispatch', function ($e) {
-			$controller = $e->getTarget ();
-			$controller->layout ( 'layout/layoutWxb' );
-		}, 100 );*/
-	}
 	public function getAutoloaderConfig() {
 		return array (
 				'Zend\Loader\StandardAutoloader' => array (
@@ -25,17 +14,5 @@ class Module {
 	}
 	public function getConfig() {
 		return include __DIR__ . '/config/module.config.php';
-	}
-	public function getServiceConfig() {
-		return array (
-				'factories' => array (
-						'Order\Service\PersistOrder' => function($sm) {
-							return new PersistOrder($sm->get('OrderDbAdapter'));
-						},
-						'Order\Service\PersistMultiOrder' => function($sm) {
-							return new PersistMultiOrder($sm->get('OrderDbAdapter'));
-						}
-				)
-		);
 	}
 }
